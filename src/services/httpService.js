b182e7b13eb9a1cdc10bb00adf6e8e460399c31a
pakzad <pakzadlive@gmail.com>
2023-08-17 17:42:26 +0330

    Create new accessToken based on refreshToken

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,36 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
 const app = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: baseURL,
   withCredentials: true,
 });
+
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${baseURL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
 const http = {
   get: app.get,
