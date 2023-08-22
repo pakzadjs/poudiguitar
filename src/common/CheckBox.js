5abb50d8d8fea7488990589c98dee0d54652f58a
pakzad <pakzadlive@gmail.com>
2023-08-22 19:15:09 +0330

    CheckBox component added from NextUI

--- src/common/CheckBox.js
+++ src/common/CheckBox.js
@@ -1,18 +1,23 @@
+import { Checkbox } from "@nextui-org/react";
+
 export default function CheckBox({ id, name, value, onChange, checked, label }) {
+  const classNames = {
+    base: "gap-x-2",
+    label: "text-slate-200",
+  };
+
   return (
     <div className="flex items-center gap-x-2 text-slate-200">
-      <input
-        type="checkbox"
+      <Checkbox
+        classNames={classNames}
         name={name}
         id={id}
         checked={checked}
         value={value}
         onChange={onChange}
-        className="cursor-pointer rounded-[5px] border-none bg-blue-100/80 w-4 h-4 checked:text-blue-900"
-      />
-      <label htmlFor={id} className="cursor-pointer">
+      >
         {label}
-      </label>
+      </Checkbox>
     </div>
   );
 }
