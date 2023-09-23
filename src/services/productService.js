c39088ad55fa88efa1e84258eac6b4b95a40c164
pakzad <pakzadlive@gmail.com>
2023-09-23 18:55:21 +0200

    Comments deleted

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,24 +2,12 @@ import http from "./httpService";
 
 export function getCourses(queryString) {
   return http.get(`/product/list?${queryString}&type=course`).then(({ data }) => data.data);
-
-  // return fetch(`${process.env.NEXT_PUBLIC_API_URL}/product/list?${queryString}`, {
-  //   cache: "no-store",
-  // })
-  //   .then((res) => res.json())
-  //   .then(({ data }) => data);
 }
 
 export function getDownloadables(queryString) {
   return http
     .get(`/product/list?${queryString}&type=downloadable`)
     .then(({ data }) => data.data);
-
-  // return fetch(`${process.env.NEXT_PUBLIC_API_URL}/product/list?${queryString}`, {
-  //   cache: "no-store",
-  // })
-  //   .then((res) => res.json())
-  //   .then(({ data }) => data);
 }
 
 export function getOneProductBySlug(slug) {
