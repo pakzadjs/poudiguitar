f85bdca64b0e7badde186e0af3e3cefe5296cbbd
pakzad <pakzadlive@gmail.com>
2023-09-23 11:38:01 +0200

    Commented codes and console.log removed

--- src/app/(user)/courses/[slug]/FAQ.js
+++ src/app/(user)/courses/[slug]/FAQ.js
@@ -8,35 +8,10 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function FAQ({ product }) {
-  console.log(product.FAQ);
   return (
     <Fragment>
       {product?.FAQ.map((q) => {
         return (
-          // <Accordion
-          // key={q?._id}
-          // sx={{
-          //   bgcolor: "#113A63",
-          //   borderRadius: "10px",
-          //   marginBottom: "5px",
-          // }}
-          // >
-          //   <AccordionSummary
-          //     expandIcon={<ExpandMoreIcon className="text-white" />}
-          //     aria-controls="panel1a-content"
-          //     id="panel1a-header"
-          //     sx={{ padding: "10px" }}
-          //   >
-          //     <Typography sx={{ fontFamily: `vazir`, color: "#dcdcdc" }}>
-          //       {q?.question}
-          //     </Typography>
-          //   </AccordionSummary>
-          //   <AccordionDetails>
-          //     <Typography sx={{ fontFamily: `vazir`, color: "#a9cbef" }}>
-          //       {q?.answer}
-          //     </Typography>
-          //   </AccordionDetails>
-          // </Accordion>
           <Accordion
             key={q?._id}
             sx={{
