0c92f0d4476f83fa9d2e97c0d1863b8cb188fb48
pakzad <pakzadlive@gmail.com>
2023-09-27 14:52:02 +0200

    Courses section without any paid courses

--- src/app/(profile)/profile/courses/page.js
+++ src/app/(profile)/profile/courses/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { cookies } from "next/headers";
 import axios from "axios";
-import { TbExclamationCircle } from "react-icons/tb";
+import { IoIosArrowBack } from "react-icons/io";
 import MyCoursesDetails from "./MyCoursesDetails";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
@@ -28,45 +28,62 @@ const getData = async (id) => {
 };
 
 async function MyCourses() {
-  const { students } = await getData();
+  const data = await getData();
+  const { students } = data || {};
 
   return (
     <div className="xl:max-w-screen-xl m-auto">
       <h2 className="text-xl font-extrabold mb-4 mr-1">دوره های من</h2>
 
       <div className="z-30 bg-blue-950/50 p-10 rounded-3xl flex items-center">
-        <div className="relative rounded-xl overflow-auto">
-          <div className="shadow-sm overflow-auto my-8">
-            <table className="border-collapse table-auto w-full min-w-[900px] text-sm">
-              <thead>
-                <tr>
-                  <th className="table__th">سفارش</th>
-                  <th className="table__th">لایسنس</th>
-                  <th className="table__th">روش دسترسی</th>
-                  <th className="table__th">کانال تلگرام دوره</th>
-                  <th className="table__th">مدرس دوره</th>
-                  <th className="table__th">تاریخ</th>
-                </tr>
-              </thead>
-              <tbody className="bg-blue-900/50">
-                {students?.map((detail) => {
-                  const { license, channel, product, createdAt, _id } = detail;
-                  return (
-                    <MyCoursesDetails
-                      license={license}
-                      channel={channel}
-                      product={product}
-                      createdAt={createdAt}
-                      key={_id}
-                    />
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+        {students ? (
+          <div className="relative rounded-xl overflow-auto">
+            <div className="shadow-sm overflow-auto my-8">
+              <table className="border-collapse table-auto w-full min-w-[900px] text-sm">
+                <thead>
+                  <tr>
+                    <th className="table__th">سفارش</th>
+                    <th className="table__th">لایسنس</th>
+                    <th className="table__th">روش دسترسی</th>
+                    <th className="table__th">کانال تلگرام دوره</th>
+                    <th className="table__th">مدرس دوره</th>
+                    <th className="table__th">تاریخ</th>
+                  </tr>
+                </thead>
+                <tbody className="bg-blue-900/50">
+                  {students?.map((detail) => {
+                    const { license, channel, product, createdAt, _id } = detail;
+                    return (
+                      <MyCoursesDetails
+                        license={license}
+                        channel={channel}
+                        product={product}
+                        createdAt={createdAt}
+                        key={_id}
+                      />
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
 
-          <div class="absolute inset-0 pointer-events-none border border-white/5 rounded-xl"></div>
-        </div>
+            <div class="absolute inset-0 pointer-events-none border border-white/5 rounded-xl"></div>
+
+            <div className="absolute left-0 top-0 h-full items-center ml-3 hidden max-xl:flex">
+              <IoIosArrowBack
+                size={30}
+                className="shadow-md rounded-full p-1 shadow-sky-800/80"
+              />
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p className="text-lg font-semibold mb-3">شما در دوره ای ثبت نام نکردید</p>
+            <Link href="/courses">
+              <button className="btn">رفتن به صفحه دوره ها</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
