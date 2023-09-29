53a1c5ffaaf0aa1aced3b2e3c7b87c8af35e1918
pakzad <pakzadlive@gmail.com>
2023-09-29 12:58:05 +0200

    Not found page

---
+++ src/app/(user)/not-found.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { TbArrowLeftBar } from "react-icons/tb";
+
+export default function NotFound() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="bg-blue-950/50 p-10 rounded-3xl min-w-xl flex flex-col items-center justify-center gap-3">
+        <h1 className="text-2xl font-bold text-red-600">صفحه موردنظر پیدا نشد</h1>
+        <Link
+          href="/"
+          className="flex items-center gap-2 hover:text-blue-500 transition-all duration-250"
+        >
+          <p> بازگشت به صفحه اصلی </p>
+          <TbArrowLeftBar />
+        </Link>
+      </div>
+    </main>
+  );
+}
