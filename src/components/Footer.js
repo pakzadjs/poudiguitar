2b5926f9a516a9082b16a1283cc9665f4503802f
pakzad <pakzadlive@gmail.com>
2023-09-30 09:50:44 +0200

    Working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
+"use client";
+
+import Link from "next/link";
+import Script from "next/script";
+
 import { BsInstagram, BsYoutube, BsTelegram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa6";
+import Zarinpal from "./Zarinpal";
 
 export default function Footer() {
   return (
     <footer className="py-6 md:py-8 text-slate-300 mt-10 bg-slate-400/30 px-4 rounded-t-3xl">
-      <div className="grid gap-10 md:gap-14 grid-cols-5 grid-row-2 container md:max-w-screen-xl mx-auto">
+      <div className="grid gap-10 md:gap-14 grid-cols-5 grid-row-2 container md:max-w-screen-xl mx-auto px-3 lg:px-20">
         <div className="col-span-5 md:col-span-2 flex flex-col justify-start">
           <div>
             <h2 className="font-bold text-xs sm:text-sm mb-4 text-white">
@@ -18,13 +25,9 @@ export default function Footer() {
 
               <p className="leading-5 md:leading-7">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از
-                طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که
-                لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
-                ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده،
-                شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای
-                طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
-                کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
-                جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطان امید داشت
+                که تمام و دشواری موجود در ارائه راهکارها، و جوابگوی سوالات پیوسته اهل دنیای
+                موجود طراحی اساسا مورد استفاده قرار گیرد.
               </p>
             </div>
           </div>
@@ -60,11 +63,7 @@ export default function Footer() {
 
               {/* Telegram */}
               <li className="block">
-                <a
-                  rel="noreferrer"
-                  href="https://www.instagram.com/poudiguitar/"
-                  target="_blank"
-                >
+                <a rel="noreferrer" href="https://t.me/poudisupport" target="_blank">
                   <BsTelegram
                     size={26}
                     className="hover:text-sky-500 transition-all duration-250"
@@ -74,11 +73,43 @@ export default function Footer() {
             </ul>
           </div>
         </div>
-        <div className="col-span-5 md:col-span-1"></div>
-        <div className="col-span-5 md:col-span-1"></div>
-        <div className="col-span-5 md:col-span-1"></div>
-        <div className="col-span-5">
-          <p className="text-center tex-xs">© تمامی حقوق برای پودی گیتار محفوظ است</p>
+
+        {/* <div className="col-span-5 md:col-span-1">div</div> */}
+
+        <div className="col-span-5 md:col-span-2 md:mr-10">
+          <h3 className="mb-4 font-bold text-white">بخش های سایت</h3>
+
+          <ul>
+            <li className="footer__list">
+              <Link href="/courses">دوره های آموزشی</Link>
+            </li>
+
+            <li className="footer__list">
+              <Link href="/downloads">دانلود ها</Link>
+            </li>
+
+            <li className="footer__list">
+              <Link href="/about">درباره ما</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Zarinpal */}
+        <div className="col-span-5 md:col-span-1">
+          <h3 className="mb-4 font-bold text-white">پرداخت امن زرین پال</h3>
+
+          <div className="border inline-block border-slate-500 p-3 rounded-lg transition-all duration-250 hover:bg-blue-950/50">
+            <Zarinpal />
+          </div>
+        </div>
+
+        {/* Copyright */}
+        <div className="col-span-5 flex items-center m-auto gap-3">
+          <p className="text-center tex-xs">© تمامی حقوق برای پودی گیتار محفوظ است.</p>
+
+          <a rel="noreferrer" href="https://github.com/pakzadjs" target="_blank">
+            <FaGithub size={25} className="hover:text-white transition-all duration-250" />
+          </a>
         </div>
       </div>
     </footer>
---
+++ src/components/Zarinpal.js
@@ -0,0 +1,16 @@
+export default function Zarinpal() {
+  return (
+    <a
+      href={`https://www.zarinpal.com/trustPage/${window.location.hostname}`}
+      title="دروازه پرداخت معتبر"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <img
+        src="https://cdn.zarinpal.com/badges/trustLogo/1.svg"
+        border="0"
+        alt="دروازه پرداخت معتبر"
+      />
+    </a>
+  );
+}
