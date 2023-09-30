e225db06f16ba08a6847c184ad8d1ab37e7fbce1
pakzad <pakzadlive@gmail.com>
2023-09-30 13:12:01 +0200

    Dynamic size

--- src/common/Spinner.js
+++ src/common/Spinner.js
@@ -2,6 +2,12 @@
 
 import { Spinner } from "@nextui-org/react";
 
-export default function SpinnerComponent() {
-  return <Spinner color="primary" size="lg" className="flex items-center justify-center" />;
+export default function SpinnerComponent({ size }) {
+  return (
+    <Spinner
+      color="primary"
+      size={size ? size : "lg"}
+      className="flex items-center justify-center"
+    />
+  );
 }
