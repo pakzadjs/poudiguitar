2286425369338c477972969687b7ac8cecdf5a16
pakzad <pakzadlive@gmail.com>
2023-10-01 20:30:00 +0200

    Dynamic list background color

--- src/app/(admin)/admin/AdminSidebar.js
+++ src/app/(admin)/admin/AdminSidebar.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { logout } from "@/services/authServices";
 import {
   TbHome,
@@ -17,13 +18,12 @@ import {
 } from "react-icons/tb";
 
 function AdminSideBar() {
+  const pathname = usePathname();
+
   const logoutHandler = async () => {
     await logout();
     document.location.href = "/";
   };
-
-  const listStyles =
-    "w-full hover:bg-slate-400/20 py-3 px-3 rounded-2xl flex items-center transition-all duration-250";
 
   return (
     <aside>
@@ -32,70 +32,96 @@ function AdminSideBar() {
       </Link>
       <ul className="flex flex-col space-y-2 pt-10 lg:pt-8">
         <li>
-          <Link href="/" className={listStyles}>
+          <Link href="/" className="sidebar__list">
             <TbHome size={20} className="ml-3" />
             صفحه اصلی
           </Link>
         </li>
 
         <li>
-          <Link href="/admin" className={listStyles}>
+          <Link
+            href="/admin"
+            className={`sidebar__list ${pathname == "/admin" && "bg-slate-500/40"}`}
+          >
             <TbSettings2 size={20} className="ml-3" />
             داشبورد
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/users" className={listStyles}>
+          <Link
+            href="/admin/users"
+            className={`sidebar__list ${pathname == "/admin/users" && "bg-slate-500/40"}`}
+          >
             <TbUsersGroup size={20} className="ml-3" />
             کاربر ها
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/students" className={listStyles}>
+          <Link
+            href="/admin/students"
+            className={`sidebar__list ${pathname == "/admin/students" && "bg-slate-500/40"}`}
+          >
             <TbUserStar size={20} className="ml-3" />
             هنرجو ها
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/products" className={listStyles}>
+          <Link
+            href="/admin/products"
+            className={`sidebar__list ${pathname == "/admin/products" && "bg-slate-500/40"}`}
+          >
             <TbBooks size={20} className="ml-3" />
             محصولات
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/categories" className={listStyles}>
+          <Link
+            href="/admin/categories"
+            className={`sidebar__list ${pathname == "/admin/categories" && "bg-slate-500/40"}`}
+          >
             <TbCategory size={20} className="ml-3" />
             دسته بندی
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/payments" className={listStyles}>
+          <Link
+            href="/admin/payments"
+            className={`sidebar__list ${pathname == "/admin/payments" && "bg-slate-500/40"}`}
+          >
             <TbList size={20} className="ml-3" />
             پرداخت ها
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/static-pages" className={listStyles}>
+          <Link
+            href="/admin/static-pages"
+            className={`sidebar__list ${
+              pathname == "/admin/static-pages" && "bg-slate-500/40"
+            }`}
+          >
             <TbFileText size={20} className="ml-3" />
             صفحه های استاتیک
           </Link>
         </li>
 
         <li>
-          <Link href="/admin/coupons" className={listStyles}>
+          <Link
+            href="/admin/coupons"
+            className={`sidebar__list ${pathname == "/admin/coupons" && "bg-slate-500/40"}`}
+          >
             <TbPercentage size={20} className="ml-3" />
             کد تخفیف
           </Link>
         </li>
 
         <li>
-          <button onClick={logoutHandler} className={listStyles}>
+          <button onClick={logoutHandler} className="sidebar__list">
             <TbLogout size={20} className="ml-3" />
             خروج از حساب
           </button>
--- src/app/(profile)/profile/SideBar.js
+++ src/app/(profile)/profile/SideBar.js
@@ -2,18 +2,19 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 import { logout } from "@/services/authServices";
-import { TbHome, TbLogout, TbBooks, TbUserEdit, TbSettings2, TbList } from "react-icons/tb";
 import { PiPasswordBold } from "react-icons/pi";
+import { TbHome, TbLogout, TbBooks, TbUserEdit, TbSettings2, TbList } from "react-icons/tb";
 
 function SideBar() {
+  const pathname = usePathname();
+
   const logoutHandler = async () => {
     await logout();
     document.location.href = "/";
   };
-
-  const listStyles =
-    "w-full hover:bg-slate-400/20 py-3 px-3 rounded-2xl flex items-center transition-all duration-250";
 
   return (
     <aside>
@@ -22,49 +23,64 @@ function SideBar() {
       </Link>
       <ul className="flex flex-col space-y-2 pt-10 lg:pt-8">
         <li>
-          <Link href="/" className={listStyles}>
+          <Link href="/" className="sidebar__list">
             <TbHome size={20} className="ml-3" />
             صفحه اصلی
           </Link>
         </li>
 
         <li>
-          <Link href="/profile" className={listStyles}>
+          <Link
+            href="/profile"
+            className={`sidebar__list ${pathname == "/profile" && "bg-slate-500/40"}`}
+          >
             <TbSettings2 size={20} className="ml-3" />
             داشبورد
           </Link>
         </li>
 
         <li>
-          <Link href="/profile/courses" className={listStyles}>
+          <Link
+            href="/profile/courses"
+            className={`sidebar__list ${pathname == "/profile/courses" && "bg-slate-500/40"}`}
+          >
             <TbBooks size={20} className="ml-3" />
             دوره ها
           </Link>
         </li>
 
         <li>
-          <Link href="/profile/payments" className={listStyles}>
+          <Link
+            href="/profile/payments"
+            className={`sidebar__list ${pathname == "/profile/payments" && "bg-slate-500/40"}`}
+          >
             <TbList size={20} className="ml-3" />
             تراکنش ها
           </Link>
         </li>
 
         <li>
-          <Link href="/profile/me" className={listStyles}>
+          <Link
+            href="/profile/me"
+            className={`sidebar__list ${pathname == "/profile/me" && "bg-slate-500/40"}`}
+          >
             <TbUserEdit size={20} className="ml-3" />
             ویرایش مشخصات
           </Link>
         </li>
 
         <li>
-          <Link href="/profile/password" className={listStyles}>
+          <Link
+            href="/profile/password"
+            className={`sidebar__list ${pathname == "/profile/password" && "bg-slate-500/40"}`}
+          >
             <PiPasswordBold size={20} className="ml-3" />
             ویرایش گذرواژه
           </Link>
         </li>
 
         <li>
-          <button onClick={logoutHandler} className={listStyles}>
+          <button onClick={logoutHandler} className="sidebar__list">
             <TbLogout size={20} className="ml-3" />
             خروج از حساب
           </button>
