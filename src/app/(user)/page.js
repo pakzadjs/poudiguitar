433b40c05dd228028ec9ecfd7b25efb8a95b964f
pakzad <pakzadlive@gmail.com>
2023-10-02 14:47:10 +0200

    Working on Hero section

--- src/app/(user)/page.js
+++ src/app/(user)/page.js
@@ -1,9 +1,25 @@
+import Image from "next/image";
 import Hero from "@/components/Hero";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
+    <main className="">
+      {/* <Image
+        src="/images/studio-hero.png"
+        alt="Hero Image"
+        width={800}
+        height={700}
+        className="absolute left-0 top-0 hidden md:block z-0"
+      /> */}
+
+      <div className="container mx-auto mt-6">
+        <Hero />
+      </div>
+
+      <div className="">
+        <Slider />
+      </div>
     </main>
   );
 }
