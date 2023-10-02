4e9e5323447234396b5415136e5d0256bc91226e
pakzad <pakzadlive@gmail.com>
2023-10-02 10:54:16 +0200

    Removing Zarinpal badge for now

--- src/components/Zarinpal.js
+++ src/components/Zarinpal.js
@@ -2,17 +2,18 @@
 
 export default function Zarinpal() {
   return (
-    <a
-      href={`https://www.zarinpal.com/trustPage/${window?.location?.hostname}`}
-      title="دروازه پرداخت معتبر"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <img
-        src="https://cdn.zarinpal.com/badges/trustLogo/1.svg"
-        border="0"
-        alt="دروازه پرداخت معتبر"
-      />
-    </a>
+    // <a
+    //   href={`https://www.zarinpal.com/trustPage/${window?.location?.hostname}`}
+    //   title="دروازه پرداخت معتبر"
+    //   target="_blank"
+    //   rel="noreferrer noopener"
+    // >
+    //   <img
+    //     src="https://cdn.zarinpal.com/badges/trustLogo/1.svg"
+    //     border="0"
+    //     alt="دروازه پرداخت معتبر"
+    //   />
+    // </a>
+    <div></div>
   );
 }
