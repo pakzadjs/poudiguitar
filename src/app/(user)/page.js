d31ac1559d858306182b3c38e0d0508da8051fc1
pakzad <pakzadlive@gmail.com>
2023-10-02 20:31:39 +0200

    Wroking on home page products slider

--- src/app/(user)/page.js
+++ src/app/(user)/page.js
@@ -1,8 +1,20 @@
-import Image from "next/image";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import queryString from "query-string";
+import { TbArrowBigLeftFilled } from "react-icons/tb";
+
+import { toStringCookies } from "@/utils/toStringCookies";
+import { getCourses } from "@/services/productService";
 import Hero from "@/components/Hero";
 import Slider from "@/components/Slider";
 
-export default function Home() {
+async function Home({ searchParams }) {
+  const cookieStore = cookies();
+  const strCookies = toStringCookies(cookieStore);
+
+  const data = await getCourses(queryString.stringify(searchParams), strCookies);
+  const { products } = data || [];
+
   return (
     <main className="">
       {/* <Image
@@ -13,13 +25,33 @@ export default function Home() {
         className="absolute left-0 top-0 hidden md:block z-0"
       /> */}
 
-      <div className="container mx-auto mt-6">
+      <div className="container mx-auto mt-6 mb-4">
         <Hero />
       </div>
 
-      <div className="">
-        <Slider />
+      <div
+        className="container rounded-xl p-3 md:p-8 bg-transparent bg-gradient-to-r from-blue-950/30 to-blue-950/30"
+        style={{
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="">
+          <div className="flex items-center justify-between">
+            <h2 className="text-2xl font-black">جدید ترین دوره ها</h2>
+            <Link href="/courses" className="btn gap-1">
+              <span>همه دوره ها</span>
+              <TbArrowBigLeftFilled size={20}/>
+            </Link>
+          </div>
+
+          <div className="flex-1 overflow-x-hidden rounded-lg overflow-hidden">
+            <Slider courses={products} />
+          </div>
+        </div>
       </div>
     </main>
   );
 }
+
+export default Home;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,22 +1,43 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import ProductCard from "@/common/ProductCard";
+import useWindowSize from "@/hooks/useWindowSize";
 
 import "swiper/css";
+import "swiper/css/navigation";
 
-export default function Slider() {
+export default function Slider({ courses }) {
+  const { width, height } = useWindowSize();
+
   return (
     <div className="">
       <Swiper
         spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={width < 400 ? 1 : width < 1024 ? 2 : width < 1280 ? 3 : 4}
+        navigation={true}
+        grabCursor={true}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper cursor-grab"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {courses.map((course, i) => {
+          return (
+            <SwiperSlide className="swiper-slide" key={i}>
+              <div className="my-20">
+                <div className="">
+                  <ProductCard product={course} />
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
