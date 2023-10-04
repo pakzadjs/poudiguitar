683da8e44556aba70f7b288798e8e123b280f06a
pakzad <pakzadlive@gmail.com>
2023-10-04 14:05:23 +0200

    Meta tag for enamad

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 
 import vazirFont from "@/constants/localFonts";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export const metadata = {
   title: "پودی گیتار | آموزش موسیقی و گیتار",
@@ -14,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
+      <Head>
+        <meta name="enamad" content="715307" />
+      </Head>
       <body className={`${vazirFont.variable} font-sans main-bg-color`}>
         <Providers>
           <Toaster />
