18913c3698f21f2f37c61d51885bf4820b60ca3c
pakzad <pakzadlive@gmail.com>
2023-10-06 13:19:04 +0200

    Admin dashboard

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -1,3 +1,9 @@
 export default function Admin() {
-  return <div>Admin Page</div>;
+  return (
+    <main className="xl:max-w-screen-xl m-auto">
+      <div className="z-30 bg-blue-950/50 p-10 rounded-3xl flex items-center">
+        <h2 className="font-bold text-xl">پنل ادمین</h2>
+      </div>
+    </main>
+  );
 }
