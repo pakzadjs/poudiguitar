2541b137b9362be713cc89eaa2a2ff6d1abb2f1e
pakzad <pakzadlive@gmail.com>
2023-10-06 15:42:30 +0200

    Zarinpal href fixed

--- src/components/Zarinpal.js
+++ src/components/Zarinpal.js
@@ -2,33 +2,33 @@
 
 export default function Zarinpal() {
   return (
-    // <a
-    //   href={`https://www.zarinpal.com/trustPage/${window?.location?.hostname}`}
-    //   title="دروازه پرداخت معتبر"
-    //   target="_blank"
-    //   rel="noreferrer noopener"
-    // >
-    //   <img
-    //     src="https://cdn.zarinpal.com/badges/trustLogo/1.svg"
-    //     border="0"
-    //     alt="دروازه پرداخت معتبر"
-    //   />
-    // </a>
+    <>
+      <a
+        href={`https://www.zarinpal.com/trustPage/poudiguitar.com`}
+        title="دروازه پرداخت معتبر"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img
+          src="https://cdn.zarinpal.com/badges/trustLogo/1.svg"
+          border="0"
+          alt="دروازه پرداخت معتبر"
+        />
+      </a>
 
-    // <a
-    //   referrerPolicy="origin"
-    //   target="_blank"
-    //   href="https://trustseal.enamad.ir/u?id=386830&code=HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
-    // >
-    //   <img
-    //     referrerPolicy="origin"
-    //     src="https://trustseal.enamad.ir/logo.aspx?id=386830&code=HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
-    //     alt=""
-    //     style={{ cursor: "pointer" }}
-    //     code="HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
-    //   />
-    // </a>
-
-    <div></div>
+      {/* <a
+        referrerPolicy="origin"
+        target="_blank"
+        href="https://trustseal.enamad.ir/u?id=386830&code=HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
+      >
+        <img
+          referrerPolicy="origin"
+          src="https://trustseal.enamad.ir/logo.aspx?id=386830&code=HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
+          alt=""
+          style={{ cursor: "pointer" }}
+          code="HqIN3GAgKs9rmyFeYFODYW8UPF0Qdipt"
+        />
+      </a> */}
+    </>
   );
 }
