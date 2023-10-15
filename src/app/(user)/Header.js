c1417909647df4b6f135a4a4e72f7b3fb2f87b45
pakzad <pakzadlive@gmail.com>
2023-10-15 11:55:57 +0200

    Burger menu UI fix

--- src/app/(user)/Header.js
+++ src/app/(user)/Header.js
@@ -315,12 +315,12 @@ function Header() {
           </NavbarContent>
 
           {/* Burger menu */}
-          <NavbarMenu className="bg-gray">
+          <NavbarMenu className="bg-gray flex flex-col gap-6 pt-7">
             {menuItems.map((item, index) => (
-              <div key={`${item}-${index}`}>
+              <div key={`${item}-${index}`} className="w-full">
                 {item.href ? (
                   <Link
-                    className="w-full hover:bg-slate-400/20 py-2 px-3 rounded-2xl "
+                    className="w-full hover:bg-slate-400/20 py-2 px-3 rounded-2xl"
                     color={
                       index === 2
                         ? "primary"
@@ -335,7 +335,12 @@ function Header() {
                     {item?.name}
                   </Link>
                 ) : (
-                  <button onClick={logoutHandler}>{item?.name}</button>
+                  <button
+                    className="hover:bg-slate-400/20 py-2 px-3 rounded-2xl"
+                    onClick={logoutHandler}
+                  >
+                    {item?.name}
+                  </button>
                 )}
               </div>
             ))}
