83c4f044a2ae125fb83d99cd7c74edb3660b1a48
pakzad <pakzadlive@gmail.com>
2023-10-15 11:57:15 +0200

    Bug fix

--- src/common/ProductCard.js
+++ src/common/ProductCard.js
@@ -76,7 +76,7 @@ export default function ProductCard({ product }) {
               size={20}
             />
             
-            {product?.type !== "course" && (
+            {product?.type === "course" && (
               <span className="bg-slate-100/90 px-1 rounded-md flex items-center text-slate-900 cursor-default">
                 <TbUsers size={19} />
                 <span className="text-sm mr-1 font-bold">
