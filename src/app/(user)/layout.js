93d1bcd491116e88048a4ab265bddacaa58e9e84
pakzad <pakzadlive@gmail.com>
2024-02-19 10:33:28 +0100

    bug

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -1,22 +1,17 @@
-"use client";
-
 import "../globals.css";
 import Header from "./Header";
 import { Providers } from "../Providers";
 import { Toaster } from "react-hot-toast";
-import { usePathname } from "next/navigation";
 
 import vazirFont from "@/constants/localFonts";
 import Footer from "@/components/Footer";
 
-// export const metadata = {
-//   title: "پودی گیتار | آموزش موسیقی و گیتار",
-//   description: "آموزش موسیقی و گیتار",
-// };
+export const metadata = {
+  title: "پودی گیتار | آموزش موسیقی و گیتار",
+  description: "آموزش موسیقی و گیتار",
+};
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-
   return (
     <html lang="fa" dir="rtl">
       <head>
@@ -27,7 +22,7 @@ export default function RootLayout({ children }) {
           <Toaster />
           <Header />
           {children}
-          {pathname !== "/auth" && <Footer />}
+          <Footer />
         </Providers>
       </body>
     </html>
